1a7ac7b4f5490ca69da1d28b5252252cae7f888c
Bokdol11859 <2019147551@yonsei.ac.kr>
2024-02-13 23:49:52 +0900

    feat: implement infinite fetch with button

--- src/api/endpoint.ts
+++ src/api/endpoint.ts
@@ -1,12 +1,16 @@
 import { Product } from "@/types/product";
 
-export const getProducts = async (): Promise<Product[]> => {
+export const getProducts = async (size: number): Promise<Product[]> => {
   try {
     const response = await fetch("/api/products", {
       method: "GET",
-    }).then((res) => res.json());
+    });
+    const jsonData = await response.json();
+    const idx = Math.max(Math.floor(Math.random() * jsonData.data.length) - size, 0);
 
-    return response;
+    const data = jsonData.data.slice(idx, idx + size);
+
+    return data;
   } catch (e) {
     throw e;
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import { getProducts } from "@/api/endpoint";
+import { Product } from "@/types/product";
 import React from "react";
 
 export default function Home() {
-  React.useEffect(() => {
-    (async () => {
-      const data = await getProducts();
-      console.log(data);
-    })();
-  }, []);
+  const [products, setProducts] = React.useState<Product[]>([]);
 
-  return <div></div>;
+  const fetchMoreProducts = async () => {
+    const data = await getProducts(10);
+    setProducts((prev) => {
+      return [...prev, ...data];
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        {products.map((product, idx) => (
+          <div key={`${product.id}-${idx}`}>{product.title}</div>
+        ))}
+      </div>
+      <button onClick={fetchMoreProducts}>fetch</button>
+    </div>
+  );
 }
